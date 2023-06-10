9bc3c129ef93f9ceb91156b5a550f974cbd6705b
Guillermo Afonso <118361589+CH0P0@users.noreply.github.com>
2023-06-10 12:54:35 +0200

    Add files via upload

---
+++ PracticaETSXII/Ejercicio1/Ejercicio1_Apartado2.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1
+{
+	internal class Ejercicio1_Apartado2
+	{
+		Product Product { get; set; }
+		string Time { get; set; }
+
+		public Order(Product Product)
+		{
+			this.Product = Product;
+			Time = GetDate();
+		}
+
+		public string GetDate()
+		{
+			DateTime actualDate = DateTime.Now;
+			return actualDate.ToString("dd/MM/yyyy HH:mm:ss");
+		}
+
+		public static bool ItIsFull(Queue c) => c.Size() == 5 ? true : false;
+
+		public static Order MakeOrder()
+		{
+			Order order;
+			int option;
+			Product.ShowProducts();
+			option = Functions.ReadInt("Elija un producto: ", 0, Product.products.Count - 1);
+			order = new Order(Product.products[option]);
+			return order;
+		}
+
+		public static decimal CountMoney(List<Order> orders)
+		{
+			decimal total = 0;
+			orders.ForEach(order =>
+			{
+				total += order.Product.GetPrice();
+			});
+			return total;
+		}
+
+		public static void ShowOrders(List<Order> orders)
+		{
+			orders.ForEach(order => {
+				Console.WriteLine(order);
+			});
+		}
+		public string GetTime() => Time;
+
+		public override string ToString() => $"producto: {Product}\tfecha-hora:{GetTime()}";
+	}
+}
---
+++ PracticaETSXII/Ejercicio2/Program.cs
@@ -0,0 +1,45 @@
+﻿using Ejercicio2;
+
+namespace Ejercicio2
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			List<Order> orders = new();
+			Queue cola = new();
+			int option;
+			Console.WriteLine("Bienvenido a la cafeteria");
+			bool value = true;
+			do
+			{
+				option = Functions.Menu();
+				switch (option)
+				{
+					case 1:
+						if (!Order.ItIsFull(cola))
+						{
+							Order o = Order.MakeOrder();
+							orders.Add(o);
+							cola.Push(o);
+						}
+						else
+							Console.WriteLine("La cola esta llena, espere o vayase.");
+						break;
+					case 2:
+						cola.Pop();
+						break;
+					default:
+						value = false;
+						break;
+				}
+				Console.ReadKey();
+				Console.Clear();
+			} while (value);
+			decimal total = Order.CountMoney(orders);
+			Order.ShowOrders(orders);
+			Console.WriteLine($"El total de dinero recaudado ha sido: {total}");
+
+		}
+	}
+}
